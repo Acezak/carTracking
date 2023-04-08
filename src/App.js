fd9ecfb2c9cc20a6afb4ade88856a2e663e9b293
Juan Esteban Acevedo Sánchez <101371132+Acezak@users.noreply.github.com>
2023-04-07 19:27:45 -0500

    Home view implemented

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import {app} from "./fb";
 import Home from "./Home";
 import Login from "./Login";
+import './stylesheets/App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
 
@@ -12,7 +14,22 @@ function App() {
     });
   }, []);
   
-  return <> {user ? <Home /> : <Login setUser = {setUser} />} </>;
+  return (
+  <div className="App">
+    <> {user ? <Home /> : <Login setUser = {setUser} />} </>
+
+    <>
+      <Router>
+        <Switch>
+          <Route path='/map' component={Map}></Route>
+          <Route path='/panel' component={Panel}></Route>
+        </Switch>
+      </Router>
+    </>
+    
+  </div>
+  )
+  
 }
 
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,56 @@
 import React from "react";
 import { app } from "./fb";
+import './stylesheets/Home.css';
+import Login from "./Login";
 
 const Home = () =>{
 
   const signOut = () =>{
     app.auth().signOut()
   }
+
+  const openMap = () =>{
+    console.log('map');
+  }
+
+  const openPanel = () =>{
+    console.log('panel');
+  }
+
   return(
-    <div>
-      <h1> Bienvenido </h1>
-      <button onClick={signOut}> Cerrar sesión </button>
+
+    <div className="general">
+      <div className="supView">
+        <h1 className="statText"> Bienvenido </h1>
+        <button className="signOutButton" onClick={signOut}> Cerrar sesión </button>
+      </div>
+
+      <div className="principal">
+
+        <div className="layout" onClick={openPanel}>
+          <img 
+            className ='imagen'
+            src={require('./images/panel.png')}
+            alt='imgPanel'
+          />
+          <label>Panel de control</label>
+
+        </div>
+
+        <div className="layout" onClick={openMap}>
+          <img 
+            className ='imagen'
+            src={require('./images/ubicacion.png')}
+            alt='imgMap'
+          />
+          <label>Mapa</label>
+
+        </div>
+
+      </div>
     </div>
+
   )
-};
+}
 
 export default Home;
